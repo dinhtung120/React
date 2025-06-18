a6685d6bd45b04118154c9ec5525376733b2979a
Lucifer <newstarvn01@gmail.com>
2025-06-18 22:50:45 +0700

    use redux toolkit 15-redux-intro

---
+++ 15-redux-intro/src/store-v1.js
@@ -0,0 +1,111 @@
+import { combineReducers, createStore } from "redux";
+const initialStateAccount = {
+  balance: 0,
+  loan: 0,
+  loanPurpose: "",
+};
+
+const initialStateCustomer = {
+  fullName: "",
+  nationalID: "",
+  createAt: "",
+};
+
+function accountReducer(state = initialStateAccount, action) {
+  switch (action.type) {
+    case "account/desposit":
+      return {
+        ...state,
+        balance: state.balance + action.payload,
+      };
+    case "account/withdraw":
+      return {
+        ...state,
+        balance: state.balance - action.payload,
+      };
+    case "account/requestLoan":
+      return {
+        ...state,
+        loan: action.payload.amount,
+        loanPurpose: action.payload.purpose,
+        balance: state.balance + action.payload.amount,
+      };
+    case "account/payLoan":
+      return {
+        ...state,
+        loan: 0,
+        loanPurpose: "",
+        balance: state.balance - action.payload,
+      };
+    default:
+      return state;
+  }
+}
+
+function customerReducer(state = initialStateCustomer, action) {
+  switch (action.type) {
+    case "customer/createCustomer":
+      return {
+        ...state,
+        fullName: action.payload.fullName,
+        nationalID: action.payload.nationalID,
+        createAt: action.payload.createAt,
+      };
+
+    case "customer/updateName":
+      return {
+        ...state,
+        fullName: action.payload,
+      };
+    default:
+      return state;
+  }
+}
+
+const rootReducer = combineReducers({
+  account: accountReducer,
+  customer: customerReducer,
+});
+
+const store = createStore(rootReducer);
+
+function desposit(amount) {
+  return {
+    type: "account/desposit",
+    payload: amount,
+  };
+}
+function withdraw(amount) {
+  return {
+    type: "account/withdraw",
+    payload: amount,
+  };
+}
+function takeLoan(amount, purpose) {
+  return {
+    type: "account/requestLoan",
+    payload: { amount, purpose },
+  };
+}
+function payLoan() {
+  return {
+    type: "account/payLoan",
+  };
+}
+
+function createCustomer(fullName, nationalID) {
+  return {
+    type: "customer/createCustomer",
+    payload: { fullName, nationalID, createAt: new Date().toISOString() },
+  };
+}
+
+function updateName(fullName) {
+  return { type: "customer/updateName", payload: fullName };
+}
+
+store.dispatch(createCustomer("John Doe", "1234567890"));
+console.log(store.getState());
+
+store.dispatch(desposit(1000));
+console.log(store.getState());
--- 15-redux-intro/src/store.js
+++ 15-redux-intro/src/store.js
@@ -1,63 +1,12 @@
-import { createStore } from "redux";
-const initialState = {
-  balance: 0,
-  loan: 0,
-  loanPurpose: "",
-};
+import { configureStore } from "@reduxjs/toolkit";
+import accountReducer from "./features/accounts/accountSlice";
+import customerReducer from "./features/customers/customerSlice";
 
-function reducer(state = initialState, action) {
-  switch (action.type) {
-    case "account/desposit":
-      return {
-        ...state,
-        balance: state.balance + action.payload,
-      };
-    case "account/withdraw":
-      return {
-        ...state,
-        balance: state.balance - action.payload,
-      };
-    case "account/requestLoan":
-      return {
-        ...state,
-        loan: action.payload.amount,
-        loanPurpose: action.payload.purpose,
-        balance: state.balance + action.payload.amount,
-      };
-    case "account/payLoan":
-      return {
-        ...state,
-        loan: 0,
-        loanPurpose: "",
-        balance: state.balance - action.payload,
-      };
-    default:
-      return state;
-  }
-}
+const store = configureStore({
+  reducer: {
+    account: accountReducer,
+    customer: customerReducer,
+  },
+});
 
-const store = createStore(reducer);
-
-function desposit(amount) {
-  return {
-    type: "account/desposit",
-    payload: amount,
-  };
-}
-function withdraw(amount) {
-  return {
-    type: "account/withdraw",
-    payload: amount,
-  };
-}
-function takeLoan(amount, purpose) {
-  return {
-    type: "account/requestLoan",
-    payload: { amount, purpose },
-  };
-}
-function payLoan() {
-  return {
-    type: "account/payLoan",
-  };
-}
+export default store;
