963ac05c4c836a201d1e8cf9cd83e1c7144640a0
Lucifer <newstarvn01@gmail.com>
2025-06-23 10:23:40 +0700

    apply redux toolkit in user 16-fast-react

--- 16-fast-react-pizza/src/features/order/CreateOrder.jsx
+++ 16-fast-react-pizza/src/features/order/CreateOrder.jsx
@@ -2,6 +2,7 @@ import { useState } from "react";
 import { Form, redirect, useActionData, useNavigation } from "react-router-dom";
 import { createOrder } from "../../services/apiRestaurant";
 import Button from "../../ui/Button";
+import { useSelector } from "react-redux";
 
 // https://uibakery.io/regex-library/phone-number
 const isValidPhone = (str) =>
@@ -34,6 +35,7 @@ const fakeCart = [
 ];
 
 function CreateOrder() {
+  const username = useSelector((state) => state.user.username);
   // const [withPriority, setWithPriority] = useState(false);
   const navigation = useNavigation();
   const isSubmitting = navigation.state === "submitting";
@@ -50,6 +52,7 @@ function CreateOrder() {
             <span className="sm:basis-30"> First Name</span>
             <input
               className="input w-96"
+              defaultValue={username}
               type="text"
               name="customer"
               required
