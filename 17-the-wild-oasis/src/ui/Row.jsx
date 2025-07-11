7a5fab0747e25ecff623952f7b3b546e95f4882b
Lucifer <newstarvn01@gmail.com>
2025-07-11 12:35:31 +0700

    Add update settings feature 17-the-wild-oasis

--- 17-the-wild-oasis/src/ui/Row.jsx
+++ 17-the-wild-oasis/src/ui/Row.jsx
@@ -1,25 +1,23 @@
 import styled, { css } from "styled-components";
 
-const Row = styled.div`
+const Row = styled.div.withConfig({
+  shouldForwardProp: (prop) => prop !== "type",
+})`
   display: flex;
 
   ${(props) =>
-    props.type === "horizontal" &&
+    (props.type || "vertical") === "horizontal" &&
     css`
       justify-content: space-between;
       align-items: center;
     `}
 
   ${(props) =>
-    props.type === "vertical" &&
+    (props.type || "vertical") === "vertical" &&
     css`
       flex-direction: column;
       gap: 1.6rem;
     `}
 `;
-
-Row.defaultProps = {
-  type: "vertical",
-};
 
 export default Row;
