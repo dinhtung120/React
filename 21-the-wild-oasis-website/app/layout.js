0ba70e0068cf6b4a43785e949e9305084cd090bb
Lucifer <newstarvn01@gmail.com>
2025-07-12 10:35:31 +0700

    Init 21-the-wild-oasis-website

---
+++ 21-the-wild-oasis-website/app/layout.js
@@ -0,0 +1,17 @@
+import Navigation from "./components/Navigation";
+
+export const metadata = {
+  title: "The Wild Oasis",
+};
+
+export default function RootLayout({ children }) {
+  return (
+    <html lang="en">
+      <body>
+        <Navigation />
+        <main>{children}</main>
+        <footer>Copyright by Lucifer</footer>
+      </body>
+    </html>
+  );
+}
