620d4f460c68ab4ad148476cec84078388201e42
Lucifer <newstarvn01@gmail.com>
2025-07-16 10:35:31 +0700

    Configure Tailwind 21-the-wild-oasis-website

--- 21-the-wild-oasis-website/app/layout.js
+++ 21-the-wild-oasis-website/app/layout.js
@@ -1,6 +1,8 @@
 import Logo from "./_components/Logo";
 import Navigation from "./_components/Navigation";
 
+import "@/app/_styles/globals.css";
+
 export const metadata = {
   title: "The Wild Oasis",
 };
@@ -8,7 +10,7 @@ export const metadata = {
 export default function RootLayout({ children }) {
   return (
     <html lang="en">
-      <body>
+      <body className="min-h-screen bg-stone-900 text-stone-100">
         <header>
           <Logo />
           <Navigation />
