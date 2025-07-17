173d15e2c46d0082a85f08de5904335153a74d8f
Lucifer <newstarvn01@gmail.com>
2025-07-17 10:35:31 +0700

    Swap font 21-the-wild-oasis-website

--- 21-the-wild-oasis-website/app/layout.js
+++ 21-the-wild-oasis-website/app/layout.js
@@ -1,8 +1,14 @@
 import Logo from "./_components/Logo";
 import Navigation from "./_components/Navigation";
 
+import { Josefin_Sans } from "next/font/google";
+
 import "@/app/_styles/globals.css";
 
+const josefin = Josefin_Sans({
+  subsets: ["latin"],
+  display: "swap",
+});
 export const metadata = {
   // title: "The Wild Oasis",
   title: {
@@ -15,7 +21,9 @@ export const metadata = {
 export default function RootLayout({ children }) {
   return (
     <html lang="en">
-      <body className="min-h-screen bg-stone-900 text-stone-100">
+      <body
+        className={`${josefin.className} min-h-screen bg-stone-900 text-stone-100`}
+      >
         <header>
           <Logo />
           <Navigation />
