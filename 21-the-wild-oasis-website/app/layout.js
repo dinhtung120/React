e0ccbbf24c9e8745acff53b790537fa438491037
Lucifer <newstarvn01@gmail.com>
2025-07-18 10:35:31 +0700

    Optimizing font 21-the-wild-oasis-website

--- 21-the-wild-oasis-website/app/layout.js
+++ 21-the-wild-oasis-website/app/layout.js
@@ -4,6 +4,7 @@ import Navigation from "./_components/Navigation";
 import { Josefin_Sans } from "next/font/google";
 
 import "@/app/_styles/globals.css";
+import Header from "./_components/Header";
 
 const josefin = Josefin_Sans({
   subsets: ["latin"],
@@ -22,14 +23,12 @@ export default function RootLayout({ children }) {
   return (
     <html lang="en">
       <body
-        className={`${josefin.className} min-h-screen bg-stone-900 text-stone-100`}
+        className={`${josefin.className} flex min-h-screen flex-col bg-stone-900 text-stone-100 antialiased`}
       >
-        <header>
-          <Logo />
-          <Navigation />
-        </header>
-        <main>{children}</main>
-        <footer>Copyright by Lucifer</footer>
+        <Header />
+        <div className="flex-1 px-8 py-12">
+          <main className="mx-auto max-w-7xl">{children}</main>
+        </div>
       </body>
     </html>
   );
